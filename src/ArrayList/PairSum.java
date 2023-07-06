f877900521d1a4c2cb1e4b9b634e054db8e3a070
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-06 22:13:04 +0530

    arraylist

--- src/ArrayList/PairSum.java
+++ src/ArrayList/PairSum.java
@@ -6,6 +6,15 @@ public class PairSum {
     // brute force Approach O(N^2)
     public static boolean pairSums(ArrayList<Integer> list, int target) {
 
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i+1; j < list.size(); j++) {
+                if (list.get(i) + list.get(j) == target){
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
 
@@ -34,6 +43,7 @@ public class PairSum {
         list.add(3);
         list.add(4);
         list.add(5);
-        System.out.println(pairSum(list, 5));
+        System.out.println(pairSum(list, 10));
+        System.out.println(pairSums(list,10));
     }
 }
